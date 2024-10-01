ee005a1f6811c97fce5aa635a981eb67795ee644
Mehmet Akif Tutal <tutalmehmetakif@gmail.com>
2024-10-01 14:06:16 +0300

    Matematiksel Operatörler gösterildi

---
+++ Matematiksel Operatörler.py
@@ -0,0 +1,18 @@
+# + = Toplama
+# - = Çıkarma
+# * = Çarpma
+# / = Bölme
+# ** = Üs
+# % = Mod
+# // = Tam Bölme
+
+a = 6
+b = 3
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a%b)
+print(a//b)
